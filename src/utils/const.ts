391b73d9d7715307a266bcac29aa46b70e235c6e
Ding Yu Lee <dingyu@logflows.com>
2022-11-10 22:31:56 +0800

    UPDATE:section4 upper effect

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -25,3 +25,47 @@ export const companyInfoList = [
         detailUrl: ''
     }
 ];
+
+export type RaceInfo = {
+    title: string;
+    desc: string;
+    startDate: string;
+    startDetail: string;
+    dueDate: string;
+    dueDetail: string;
+};
+
+export const raceInfoList: Array<RaceInfo> = [
+    {
+        title: 'SIGN UP!',
+        desc: '開放報名',
+        startDate: '10/13',
+        startDetail: '(四)早上 11:00',
+        dueDate: '11/06',
+        dueDetail: '(日)晚上 23:59'
+    },
+    {
+        title: 'START!',
+        desc: '個組別開賽',
+        startDate: '10/31',
+        startDetail: 'UI組、團體組開賽',
+        dueDate: '11/07',
+        dueDetail: '前端組開賽'
+    },
+    {
+        title: 'UP LOAD!',
+        desc: '登錄作品',
+        startDate: '10/31',
+        startDetail: '(一)中午 12:00',
+        dueDate: '11/28',
+        dueDetail: '(一)中午 12:00'
+    },
+    {
+        title: 'STREAM!',
+        desc: '線上直播',
+        startDate: '11/03 ',
+        startDetail: '每週四',
+        dueDate: '11/24',
+        dueDetail: ''
+    }
+];
